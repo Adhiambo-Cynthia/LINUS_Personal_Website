5f575af635430f21ee6ef020a565af7d14c238dd
Adhiambo_C <cinadhiambo22@gmail.com>
2020-03-05 23:44:23 +0300

    changes

--- pdf_gen.js
+++ pdf_gen.js
@@ -1,23 +1,35 @@
-const url='my_pdf.pdf'
+const url='sample.pdf'
 let pdfDoc=null,
     pageNum=1,
     pageRendering=false,
     pageNumLoading=null
-const scale=1.8,
-    canvas=document.querySelector("#page_render"),
-    ctx= canvas.getContext("2d") 
+const scale=1.8
+//get the document //This is where you start
+pdfjsLib.getDocument(url).promise.then(pdfDoc_=>{pdfDoc=pdfDoc_   //Set PDFJS global object (so we can easily access in our page functions
+    
+    document.getElementById("page_count").textContent=pdfDoc.numPages
+    Renderpage(pageNum)
+}
+)
+     
     //render page by numbers  
 const Renderpage= num=>{
     pageRendering=true
     //get page
     pdfDoc.getPage(num).then(page=>{
-    //set scale
-    const viewport=page.getViewport({scale})
+    
+        //set scale  //We'll create a canvas for each page to draw it on
+    const viewport=page.getViewport({scale}),    //This gives us the page's dimensions at full scale
+        canvas=document.querySelector("#page_render"),
+        ctx= canvas.getContext("2d") 
             canvas.height=viewport.height
             canvas.width=viewport.width
+           
+            //Draw it on the canvas
     const renderctx={
         canvasContext:ctx,viewport
-            } 
+            }                               //alternatively,page.render({canvasContext: context, viewport: viewport});
+
     page.render(renderctx).promise.then(()=>{
      pageRendering=false
     if(pageNumLoading!==null) {
@@ -57,13 +69,6 @@ const next_page= ()=>{
 }
 
 
-//get the document
-pdfjsLib.getDocument(url).promise.then(pdfDoc_=>{pdfDoc=pdfDoc_
-    console.log(pdfDoc)
-    document.getElementById("page_count").textContent=pdfDoc.numPages
-    Renderpage(pageNum)
-}
-    )
     //button events
  document.querySelector("#prev_page").addEventListener('click', prev_page)   
  document.querySelector("#next_page").addEventListener('click', next_page) 
